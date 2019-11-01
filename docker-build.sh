3b369f1a94932f75e9e07c19831819ddeb7a3639
BenSlabbert <benjamin.slabbert@gmail.com>
2019-11-01 15:40:03 +0200

    adding worker dockerfile updating build script

--- docker-build.sh
+++ docker-build.sh
@@ -5,6 +5,7 @@ VERSION=1.0.1-$(date '+%Y-%m-%d_%H-%M-%S')
 echo "Building Images with version: ${VERSION}"
 
 docker build -t benjaminslabbert/trak_gateway:"${VERSION}" -f ./apps/ui-server/Dockerfile .
+docker build -t benjaminslabbert/trak_worker:"${VERSION}" -f ./apps/worker/takealot/Dockerfile .
 
 echo "#########################"
 echo "# Pushing to Docker hub #"
@@ -13,3 +14,4 @@ echo "#########################"
 echo "$DOCKER_HUB_PASSWORD" | docker login -u "$DOCKER_HUB_USERNAME" --password-stdin
 
 docker push benjaminslabbert/trak_gateway:"${VERSION}"
+docker push benjaminslabbert/trak_worker:"${VERSION}"
