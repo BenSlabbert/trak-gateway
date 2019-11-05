7b9edd0b9100ea9145eb2d3bd545e154559a632e
BenSlabbert <benjamin.slabbert@gmail.com>
2019-11-05 14:45:28 +0200

    adding latest tags

--- docker-build.sh
+++ docker-build.sh
@@ -4,9 +4,9 @@ VERSION=2.0.0-${TRAVIS_BUILD_NUMBER}
 
 echo "Building Images with version: ${VERSION} travis build number: ${TRAVIS_BUILD_NUMBER}"
 
-docker build -t benjaminslabbert/trak_gateway:"${VERSION}" -f ./apps/ui-server/Dockerfile .
-docker build -t benjaminslabbert/trak_worker:"${VERSION}" -f ./apps/worker/takealot/Dockerfile .
-docker build -t benjaminslabbert/trak_search:"${VERSION}" -f ./apps/search/sonic/Dockerfile .
+docker build -t benjaminslabbert/trak_gateway:"${VERSION}" -t benjaminslabbert/trak_gateway:latest -f ./apps/ui-server/Dockerfile .
+docker build -t benjaminslabbert/trak_worker:"${VERSION}" -t benjaminslabbert/trak_worker:latest -f ./apps/worker/takealot/Dockerfile .
+docker build -t benjaminslabbert/trak_search:"${VERSION}" -t benjaminslabbert/trak_search:latest -f ./apps/search/sonic/Dockerfile .
 
 echo "#########################"
 echo "# Pushing to Docker hub #"
@@ -14,6 +14,6 @@ echo "#########################"
 
 echo "$DOCKER_HUB_PASSWORD" | docker login -u "$DOCKER_HUB_USERNAME" --password-stdin
 
-docker push benjaminslabbert/trak_gateway:"${VERSION}"
-docker push benjaminslabbert/trak_worker:"${VERSION}"
-docker push benjaminslabbert/trak_search:"${VERSION}"
+docker push benjaminslabbert/trak_gateway:"${VERSION}" benjaminslabbert/trak_gateway:latest
+docker push benjaminslabbert/trak_worker:"${VERSION}" benjaminslabbert/trak_worker:latest
+docker push benjaminslabbert/trak_search:"${VERSION}" benjaminslabbert/trak_search:latest
