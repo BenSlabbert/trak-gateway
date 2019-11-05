87c3f533960e82546b2c8bc0632c0ffde2d56835
BenSlabbert <benjamin.slabbert@gmail.com>
2019-11-05 14:51:30 +0200

    fixing push

--- docker-build.sh
+++ docker-build.sh
@@ -14,6 +14,11 @@ echo "#########################"
 
 echo "$DOCKER_HUB_PASSWORD" | docker login -u "$DOCKER_HUB_USERNAME" --password-stdin
 
-docker push benjaminslabbert/trak_gateway:"${VERSION}" benjaminslabbert/trak_gateway:latest
-docker push benjaminslabbert/trak_worker:"${VERSION}" benjaminslabbert/trak_worker:latest
-docker push benjaminslabbert/trak_search:"${VERSION}" benjaminslabbert/trak_search:latest
+docker push benjaminslabbert/trak_gateway:"${VERSION}"
+docker push benjaminslabbert/trak_gateway:latest
+
+docker push benjaminslabbert/trak_worker:"${VERSION}"
+docker push benjaminslabbert/trak_worker:latest
+
+docker push benjaminslabbert/trak_search:"${VERSION}"
+docker push benjaminslabbert/trak_search:latest
