ff391434c8d030d6313e3dd86e4bca2e1ef30fe4
BenSlabbert <benjamin.slabbert@gmail.com>
2019-11-10 19:40:02 +0200

    rename to gateway

--- docker-build.sh
+++ docker-build.sh
@@ -4,7 +4,7 @@ VERSION=2.0.0-${TRAVIS_BUILD_NUMBER}
 
 echo "Building Images with version: ${VERSION} travis build number: ${TRAVIS_BUILD_NUMBER}"
 
-docker build -t benjaminslabbert/trak_gateway:"${VERSION}" -t benjaminslabbert/trak_gateway:latest -f ./apps/ui-server/Dockerfile .
+docker build -t benjaminslabbert/trak_gateway:"${VERSION}" -t benjaminslabbert/trak_gateway:latest -f ./apps/gateway/Dockerfile .
 docker build -t benjaminslabbert/trak_worker:"${VERSION}" -t benjaminslabbert/trak_worker:latest -f ./apps/worker/takealot/Dockerfile .
 docker build -t benjaminslabbert/trak_search:"${VERSION}" -t benjaminslabbert/trak_search:latest -f ./apps/search/sonic/Dockerfile .
 
